9bcd3e05d2a1064ea5beecb037131b3d04f241bc
Brian <brianliu1319@gmail.com>
2025-03-21 16:36:49 -0700

    buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import card from './images/card.svg';
 
-const content = document.querySelector('#content');
+
+const createHeader = function () {
+    const content = document.querySelector('#content');
+
+    const header = document.createElement('header');
+    const nav = document.createElement('nav');
+
+    const menuButton = document.createElement('button');
+    const aboutButton = document.createElement('button');
+    const homeButton = document.createElement('button');
+
+    menuButton.id = "menu";
+    homeButton.id = "home";
+    aboutButton.id = "about";
+
+    nav.appendChild(homeButton)
+    nav.appendChild(menuButton);
+    nav.appendChild(aboutButton);
+
+    header.appendChild(nav);
+    content.appendChild(header);
+
+    menuButton.textContent = "Menu";
+    homeButton.textContent = "Home";
+    aboutButton.textContent = "About";
+}
+
+createHeader();
+
+const homeButton = document.querySelector("#home");
+const aboutButton = document.querySelector("#about");
+const menuButton = document.querySelector("#menu");
+
+
+homeButton.addEventListener
 
 
 
