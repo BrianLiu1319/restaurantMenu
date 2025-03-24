22576acac0998cf629b563f7da318e8d25d696a6
Brian <brianliu1319@gmail.com>
2025-03-24 16:32:56 -0700

    add about stuffs

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,11 +2,26 @@ import eevee from "../images/drinks/Eevee_Latte.png"
 
 const menuPage = () => {
     const contSelector = document.querySelector("#container");
+    const itemCont = document.createElement("div");
+    itemCont.id = "itemCont";
+    
+    contSelector.appendChild(itemCont);
 
-    const imgElement = document.createElement("img");
-    imgElement.src = eevee;
+    const item = document.createElement("div");
+    item.id = "item";
 
-    contSelector.appendChild(imgElement);
+    const card_cont = document.createElement("ul");
+    const card = document.createElement("li");
+
+    const img = document.createElement("img");
+    img.src = eevee;
+
+    card.appendChild(img);
+    card_cont.appendChild(card);
+    itemCont.appendChild(item);
+    item.append(card_cont);
+    
+    
 }
 
 export {menuPage};
