bc6c1749f3bd1e409ebeebc6c4307b428de6054b
Brian <brianliu1319@gmail.com>
2025-03-26 15:20:39 -0700

    Deployment commit

---
+++ 1e83b65d4e8ccfff0fab.js
@@ -0,0 +1,32 @@
+import "../style/homeStyle.css";
+
+import pokeTitle from "../images/pokemonTitle.png";
+
+const homePage = () => {
+    const containerDiv = document.querySelector("#container");
+    containerDiv.setAttribute("class", "home");
+
+    const contSelector = document.querySelector("#container");
+    const itemsSelector = document.createElement("div");
+    itemsSelector.id = "items";
+
+    contSelector.appendChild(itemsSelector);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = pokeTitle;
+    imgElement.alt = "pokemon cafe";
+
+    const descDiv = document.createElement("div");
+
+    itemsSelector.appendChild(imgElement);
+    itemsSelector.appendChild(descDiv);
+
+    descDiv.id = "description";
+
+    descDiv.textContent =
+        "Welcome to the Pokemon Cafe! Enjoy original sweets, \
+    drinks and meals inspired by Pokemon while surrounded by \
+    your favorite Pokemon!";
+};
+
+export { homePage };
